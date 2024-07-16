e79a227e686efac95737c30daeaa052683a9a2fa
lylia <lyliazheng@gmail.com>
2024-07-15 21:20:15 -0700

    I committed

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/LyliaOpmode.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name="eeee")
+public class LyliaOpmode extends LinearOpMode{
+    @Override
+    public void runOpMode() {
+        DcMotor myMotor = hardwareMap.get(DcMotor.class, "myMotor");
+        waitForStart();
+        double x = 0;
+        while (opModeIsActive()){
+            myMotor.setPower(Math.sin(x));
+            x+=0.00001;
+        }
+    }
+}
