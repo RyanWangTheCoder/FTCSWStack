92b8edcc962916a0f9b2193237aa1a734a6f07f4
ryanw <ryan.wang2020@gmail.com>
2024-07-15 21:13:06 -0700

    first opmode :0000000 planning on adding drive train code :0000000000 - ryan

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MyOpmode.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp (name = "Drivetrain sample code")
+public class MyOpmode extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        waitForStart();
+    }
+}
