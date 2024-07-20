de5813f884e9cfd1bf8133c3d415eca92c2819f1
Lucas Ding <lucas.l.ding@gmail.com>
2024-07-19 22:03:49 -0700

    Added hardwareQueue and PriorityMotor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/opmodes/LucasOpModes.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/opmodes/LucasOpModes.java
@@ -2,18 +2,30 @@ package org.firstinspires.ftc.teamcode.utils.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.teamcode.utils.priority.HardwareQueue;
+import org.firstinspires.ftc.teamcode.utils.priority.PriorityMotor;
 
 @TeleOp (name="My Cool Opmode")
 public class LucasOpModes extends LinearOpMode {
     @Override
     public void runOpMode() {
-        DcMotor myMotor = hardwareMap.get(DcMotor.class, "MyMotor");
+        HardwareQueue hardwareQueue = new HardwareQueue();
+        PriorityMotor myMotor = new PriorityMotor(
+                hardwareMap.get(DcMotorEx.class, "myMotor"),
+                "myMotor",
+                5,
+                2,
+                1
+        );
+        hardwareQueue.addDevice(myMotor);
         waitForStart();
         double x = 0;
         while (opModeIsActive()) {
-            myMotor.setPower(Math.sin(x));
-            x += 0.00001;
+            myMotor.setTargetPower(Math.sin(x));
+            x += 0.000001;
+            hardwareQueue.update();
         }
     }
 }
