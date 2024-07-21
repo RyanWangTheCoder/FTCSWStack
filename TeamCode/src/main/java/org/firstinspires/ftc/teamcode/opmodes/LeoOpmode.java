c482bb5e3c9424701e3c0c3d712c40ed1417a309
Sabrina Meng <sabrina.meng@gmail.com>
2024-07-20 20:54:58 -0700

    I created LeoOpmode and did the drivetrain and hardwarequeue.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/LeoOpmode.java
@@ -0,0 +1,58 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.teamcode.utils.priority.PriorityMotor;
+
+@TeleOp
+public class LeoOpmode extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        /*DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);*/
+        HardwareQueue hardwarequeue = new HardwareQueue();
+        PriorityMotor frontLeftMotor = new PriorityMotor((DCMotorEx) hardwareMap.dcMotor.get("frontLeftMotor"), "frontLeftMotor", 1, 2, 1);
+        PriorityMotor backLeftMotor = new PriorityMotor((DCMotorEx) hardwareMap.dcMotor.get("backLeftMotor"), "backLeftMotor", 1, 2, 1);
+        PriorityMotor frontRightMotor = new PriorityMotor((DCMotorEx) hardwareMap.dcMotor.get("frontRightMotor"), "frontRightMotor", 1, 2, -1);
+        PriorityMotor backRightMotor = new PriorityMotor((DCMotorEx) hardwareMap.dcMotor.get("backRightMotor"), "backRightMotor", 1, 2, -1);
+
+        hardwareQueue.addDevice(frontLeftMotor);
+        hardwareQueue.addDevice(backLeftMotor);
+        hardwareQueue.addDevice(frontRightMotor);
+        hardwareQueue.addDevice(backRightMotor);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            frontLeftMotor.setTargetPower(frontLeftPower);
+            backLeftMotor.setTargetPower(backLeftPower);
+            frontRightMotor.setTargetPower(frontRightPower);
+            backRightMotor.setTargetPower(backRightPower);
+
+            harwarequeue.update();
+        }
+    }
+}
