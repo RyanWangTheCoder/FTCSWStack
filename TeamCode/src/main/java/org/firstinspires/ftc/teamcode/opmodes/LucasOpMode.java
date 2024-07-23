15c5809a1f0636962da31eade5746d5ae2d10540
Lucas Ding <lucas.l.ding@gmail.com>
2024-07-22 20:33:03 -0700

    changes made & added pid class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/LucasOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/LucasOpMode.java
@@ -1,31 +1,76 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.utils.priority.HardwareQueue;
 import org.firstinspires.ftc.teamcode.utils.priority.PriorityMotor;
 
 @TeleOp (name="My Cool Opmode")
-public class LucasOpModes extends LinearOpMode {
+public class LucasOpMode extends LinearOpMode {
     @Override
     public void runOpMode() {
-        HardwareQueue hardwareQueue = new HardwareQueue();
-        PriorityMotor myMotor = new PriorityMotor(
-                hardwareMap.get(DcMotorEx.class, "myMotor"),
-                "myMotor",
-                5,
-                2,
-                1
-        );
-        hardwareQueue.addDevice(myMotor);
-        waitForStart();
-        double x = 0;
+        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        IMU imu = hardwareMap.get(IMU.class, "robot");
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+        imu.initialize(parameters);
+        if (isStopRequested()) return;
+
         while (opModeIsActive()) {
-            myMotor.setTargetPower(Math.sin(x));
-            x += 0.000001;
-            hardwareQueue.update();
+            double y = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x;
+            double rx = gamepad1.right_stick_x;
+            if (gamepad1.options) {
+                imu.resetYaw();
+            }
+
+            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+            rotX = rotX * 1.1;  // Counteract imperfect strafing
+
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double frontLeftPower = (rotY + rotX + rx) / denominator;
+            double backLeftPower = (rotY - rotX + rx) / denominator;
+            double frontRightPower = (rotY - rotX - rx) / denominator;
+            double backRightPower = (rotY + rotX - rx) / denominator;
+
+            frontLeftMotor.setPower(frontLeftPower);
+            backLeftMotor.setPower(backLeftPower);
+            frontRightMotor.setPower(frontRightPower);
+            backRightMotor.setPower(backRightPower);
+
+            HardwareQueue hardwareQueue = new HardwareQueue();
+            PriorityMotor myMotor = new PriorityMotor(
+                    hardwareMap.get(DcMotorEx.class, "myMotor"),
+                    "myMotor",
+                    5,
+                    2,
+                    1
+            );
+            hardwareQueue.addDevice(myMotor);
+            waitForStart();
+            double x = 0;
+            while (opModeIsActive()) {
+                myMotor.setTargetPower(Math.sin(x));
+                x += 0.000001;
+                hardwareQueue.update();
+            }
         }
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/LucasPID.java
@@ -0,0 +1,5 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+public class LucasPID {
+    //hi guys i didnt add anything to this yet
+}
