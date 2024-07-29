df9847c9237a752b2d6123978da781c2510b89aa
Lucas Ding <lucas.l.ding@gmail.com>
2024-07-28 17:47:18 -0700

    changed the opmode from field centric to robot centric, deleted imu

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/LucasOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/LucasOpMode.java
@@ -1,75 +1,65 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.IMU;
-
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.utils.priority.HardwareQueue;
 import org.firstinspires.ftc.teamcode.utils.priority.PriorityMotor;
 
 @TeleOp (name="My Cool Opmode")
 public class LucasOpMode extends LinearOpMode {
-    @Override
+
     public void runOpMode() {
-        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
-        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
-        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
-        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
-        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        IMU imu = hardwareMap.get(IMU.class, "robot");
-        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-                RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
-        imu.initialize(parameters);
-        if (isStopRequested()) return;
+        @TeleOp
+        class MecanumTeleOp extends LinearOpMode {
+            @Override
+            public void runOpMode() throws InterruptedException {
+                DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
+                DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
+                DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
+                DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
 
-        while (opModeIsActive()) {
-            double y = -gamepad1.left_stick_y;
-            double x = gamepad1.left_stick_x;
-            double rx = gamepad1.right_stick_x;
-            if (gamepad1.options) {
-                imu.resetYaw();
-            }
+                frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+                backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
-            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+                waitForStart();
 
-            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
-            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+                if (isStopRequested()) return;
 
-            rotX = rotX * 1.1;
+                while (opModeIsActive()) {
+                    double y = -gamepad1.left_stick_y;
+                    double x = gamepad1.left_stick_x * 1.1;
+                    double rx = gamepad1.right_stick_x;
+                    double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+                    double frontLeftPower = (y + x + rx) / denominator;
+                    double backLeftPower = (y - x + rx) / denominator;
+                    double frontRightPower = (y - x - rx) / denominator;
+                    double backRightPower = (y + x - rx) / denominator;
 
-            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
-            double frontLeftPower = (rotY + rotX + rx) / denominator;
-            double backLeftPower = (rotY - rotX + rx) / denominator;
-            double frontRightPower = (rotY - rotX - rx) / denominator;
-            double backRightPower = (rotY + rotX - rx) / denominator;
+                    frontLeftMotor.setPower(frontLeftPower);
+                    backLeftMotor.setPower(backLeftPower);
+                    frontRightMotor.setPower(frontRightPower);
+                    backRightMotor.setPower(backRightPower);
 
-            frontLeftMotor.setPower(frontLeftPower);
-            backLeftMotor.setPower(backLeftPower);
-            frontRightMotor.setPower(frontRightPower);
-            backRightMotor.setPower(backRightPower);
-
-            HardwareQueue hardwareQueue = new HardwareQueue();
-            PriorityMotor myMotor = new PriorityMotor(
-                    hardwareMap.get(DcMotorEx.class, "myMotor"),
-                    "myMotor",
-                    5,
-                    2,
-                    1
-            );
-            hardwareQueue.addDevice(myMotor);
-            waitForStart();
-            double x1 = 0;
-            while (opModeIsActive()) {
-                myMotor.setTargetPower(Math.sin(x));
-                x1 += 0.000001;
-                hardwareQueue.update();
+                    HardwareQueue hardwareQueue = new HardwareQueue();
+                    PriorityMotor myMotor = new PriorityMotor(
+                            hardwareMap.get(DcMotorEx.class, "myMotor"),
+                            "myMotor",
+                            5,
+                            2,
+                            1
+                    );
+                    hardwareQueue.addDevice(myMotor);
+                    waitForStart();
+                    double x1 = 0;
+                    while (opModeIsActive()) {
+                        myMotor.setTargetPower(Math.sin(x));
+                        x1 += 0.000001;
+                        hardwareQueue.update();
+                    }
+                }
             }
         }
     }
