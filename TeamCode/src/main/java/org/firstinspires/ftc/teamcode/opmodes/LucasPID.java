47fe4529f66cda8c2830d2e8127b9d772b17513e
Lucas Ding <lucas.l.ding@gmail.com>
2024-08-05 21:14:48 -0700

    fixed PID, also the last commit was for opmode not PID sorry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/LucasPID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/LucasPID.java
@@ -1,23 +1,52 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
-import com.qualcomm.robotcore.util.ElapsedTime;
+import com.acmerobotics.dashboard.config.Config;
 
-public class LucasPID {{
-        double p = 0;
-        double i = 0;
-        double d = 0;
-        double reference = 0;
-        double error = 0;
-        double motorPosition = 0;
+import org.firstinspires.ftc.teamcode.utils.Utils;
+
+@Config
+public class LucasPID {
+        public double p;
+        public double i;
+        public double d;
+        public void PID(double P, double I, double D){
+                p=P;
+                i=I;
+                d=D;
+        }
+
+        long lastLoopTime = System.nanoTime();
         double lastError = 0;
-        double integralSum = 0;
+        double integral = 0;
+        int counter = 0;
+        double loopTime = 0.00000000000000000000000000000000000000000000000000000000000000000000000;
 
-        ElapsedTime time = new ElapsedTime();
-        error = reference - motorPosition;
-        integralSum = integralSum + (error * time.seconds() );
-        double derivative = (error - lastError) / time.seconds();
-        double out = error * p + integralSum * i + derivative * d;
+        public void resetIntegral(){
+                integral=0;
+        }
+
+        public double update(double error, double min, double max){
+        if (counter == 0) {
+                lastLoopTime = System.nanoTime() - 10000000;
+        }
+
+        long currentTime = System.nanoTime();
+        loopTime = (currentTime - lastLoopTime)/1000000000.0;
+        lastLoopTime = currentTime;
+
+        double proportion = p * error;
+        integral += error * i * loopTime;
+        double derivative = d * (error - lastError)/loopTime;
+
         lastError = error;
-        time.reset();
-    }
+        counter ++;
+
+        return Utils.minMaxClip(proportion + integral + derivative, min, max);
+        }
+
+        public void updatePID(double p, double i, double d){
+                this.p = p;
+                this.i = i;
+                this.d = d;
+        }
 }
