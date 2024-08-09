d1f2593f9759357463b69949a3a259d0b03ea771
ryanw <ryan.wang2020@gmail.com>
2024-08-08 18:12:10 -0700

    i think I finished slides class, not sure if it's right or not.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/RyanSlides.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/RyanSlides.java
@@ -1,5 +1,44 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-public class RyanSlides {
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.teamcode.utils.Utils;
+import org.firstinspires.ftc.teamcode.utils.priority.HardwareQueue;
+import org.firstinspires.ftc.teamcode.utils.priority.PriorityMotor;
+
+@Config
+public class RyanSlides {
+    public enum MotorState {
+        ON,
+        OFF,
+    }
+    public PriorityMotor slidesMotor;
+    public final PriorityMotor slides;
+    public MotorState motorState;
+    public static final double KP = 1;
+    public static final double powerConstantTerm = 0.05;
+    public static double slidesPower = 1.0;
+    private double targetPosition;
+
+    public RyanSlides(HardwareMap hardwareMap, HardwareQueue hardwareQueue) {
+        slides = new PriorityMotor(hardwareMap.get(DcMotorEx.class, "slides"), "slides", 1, 2, -1);
+        this.motorState = MotorState.OFF;
+        hardwareQueue.addDevice(slides);
+    }
+
+    public void update() {
+        switch (motorState) {
+            case ON:
+                double currentPosition = slidesMotor.motor[0].getCurrentPosition();
+                slidesMotor.setTargetPower(slidesPower);
+                slides.setTargetPower(Utils.minMaxClip((targetPosition - currentPosition) * KP+powerConstantTerm, -1.0, 1.0));
+                break;
+            case OFF:
+                slides.setTargetPower(0.0);
+                break;
+
+        }
+    }
 }
