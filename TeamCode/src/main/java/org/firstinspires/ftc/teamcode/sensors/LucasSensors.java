c7193fee71c45865f63b78117eca549742f634ca
Lucas Ding <lucas.l.ding@gmail.com>
2024-08-12 20:16:45 -0700

    created drivetrain file but not started yet, sensors changed a bit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sensors/LucasSensors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sensors/LucasSensors.java
@@ -1,36 +1,19 @@
-// still
-// working
-// on
-// it,
-// dont
-// really
-// know
-// what
-// to
-// do
-// because
-// i
-// wasnt
-// here
-// when
-// eric
-// taught
-// this
-
 package org.firstinspires.ftc.teamcode.sensors;
 
 import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
 
 import org.firstinspires.ftc.teamcode.utils.priority.HardwareQueue;
 
 public class LucasSensors {
-    private LynxModule hub;
+    private LynxModule controlHub, expansionHub;
     private final HardwareQueue hardwareQueue;
     private final HardwareMap hardwareMap;
-    double slidesEncoder = 0;
-    double slidesVelocity = 0;
-    boolean isSlidesDown = false;
+
+    private double slidesEncoder;
+    private double slidesVelocity;
+    boolean isSlidesDown;
 
     public LucasSensors (HardwareMap hardwareMap, HardwareQueue hardwareQueue) {
         this.hardwareMap = hardwareMap;
