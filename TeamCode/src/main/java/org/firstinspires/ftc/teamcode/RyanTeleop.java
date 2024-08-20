c35afd4b9a267418b26b1f9570d89e3063e9d938
ryanw <ryan.wang2020@gmail.com>
2024-08-20 13:46:43 -0700

    finished teleop with deposit. haven't incorporated intake into it yet cause haven't created an fsm for that yet

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RyanTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RyanTeleop.java
@@ -6,6 +6,8 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
+import org.firstinspires.ftc.teamcode.utils.ButtonToggle;
+
 @TeleOp
 public class RyanTeleop extends LinearOpMode {
     @Override
@@ -13,6 +15,33 @@ public class RyanTeleop extends LinearOpMode {
         RyanRobot robot = new RyanRobot(hardwareMap);
         // Declare our motors
         // Make sure your ID's match your configuration
+
+        // DRIVER 1
+        ButtonToggle rightBumper_1 = new ButtonToggle();
+        ButtonToggle leftBumper_1 = new ButtonToggle();
+        ButtonToggle x_1 = new ButtonToggle();
+        ButtonToggle b_1 = new ButtonToggle();
+        ButtonToggle y_1 = new ButtonToggle();
+        ButtonToggle a_1 = new ButtonToggle();
+        ButtonToggle leftTrigger_1 = new ButtonToggle();
+        ButtonToggle rightTrigger_1 = new ButtonToggle();
+        ButtonToggle leftTrigger_1_double = new ButtonToggle();
+        ButtonToggle up_dpad_1 = new ButtonToggle();
+        ButtonToggle down_dpad_1 = new ButtonToggle();
+
+        // DRIVER 2
+        ButtonToggle dpadUp_2 = new ButtonToggle();
+        ButtonToggle dpadDown_2 = new ButtonToggle();
+        ButtonToggle dpadLeft_2 = new ButtonToggle();
+        ButtonToggle dpadRight_2 = new ButtonToggle();
+        ButtonToggle leftTrigger_2 = new ButtonToggle();
+        ButtonToggle rightTrigger_2 = new ButtonToggle();
+        ButtonToggle leftBumper_2 = new ButtonToggle();
+        ButtonToggle rightBumper_2 = new ButtonToggle();
+        ButtonToggle a_2 = new ButtonToggle();
+        ButtonToggle x_2 = new ButtonToggle();
+        ButtonToggle y_2 = new ButtonToggle();
+
         while (opModeInInit()) {
             robot.reset();
 
@@ -22,7 +51,27 @@ public class RyanTeleop extends LinearOpMode {
         while(opModeIsActive()) {
             robot.drivetrain.drive(gamepad1);
 
-            
+            if(x_1.isClicked(gamepad1.x)){
+                robot.startDepositSetup();
+            }
+
+            if(leftBumper_1.isClicked(gamepad1.left_bumper)){
+                robot.startDepositBalls();
+            }
+            if(y_1.isClicked(gamepad1.y)){
+                robot.reset();
+            }
+            if(rightBumper_1.isClicked(gamepad1.right_bumper)){
+                robot.rotateWrist();
+            }
+            if(up_dpad_1.isHeld(gamepad1.dpad_up, 0.1)){
+                robot.slides.setTargetPosition(robot.slides.getCurrPos() + 0.5);
+            }
+            if(down_dpad_1.isHeld(gamepad1.dpad_down, 0.1)){
+                robot.slides.setTargetPosition(robot.slides.getCurrPos() - 0.5);
+            }
+
+            robot.update();
         }
 
     }
